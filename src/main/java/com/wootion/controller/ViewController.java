aa658b04eaec7fc0b12d2db216753530575d3164
chenyuncheng <1032699372@qq.com>
2019-03-22 21:23:29 +0800

    Initial commit

---
+++ src/main/java/com/wootion/controller/ViewController.java
@@ -0,0 +1,19 @@
+package com.wootion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
+
+/**
+ * 视图Controller
+ * @author Chencheng
+ * @create 2017-12-13 20:44
+ **/
+@Controller
+@RequestMapping("/user")
+public class ViewController {
+
+    private Logger logger = Logger.getLogger("ViewController");
+
+}
